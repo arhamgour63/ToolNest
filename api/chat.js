e71ab16e19176794cc2e28ad69f3d07473895adf
arhamgour63 <arhamgour766@gmail.com>
2026-07-07 23:15:36 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,15 +1,5 @@
 export default function handler(req, res) {
-
-  if (req.method !== "POST") {
-    return res.status(405).json({
-      error: "Method not allowed"
-    });
-  }
-
-  const message = req.body.message || "";
-
   res.status(200).json({
-    reply: "You asked: " + message
+    reply: "ToolNest AI backend is working!"
   });
-
 }
